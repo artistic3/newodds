21f12b0b90947b648987b0a8714fc87e1ca77e25
artistic3 <artisticsoftware3@gmail.com>
2023-12-06 12:39:12 +0000

    ...

--- 1206/1.php
+++ 1206/1.php
@@ -56,8 +56,8 @@ return [
 		/**
 		Race 5
 		*/
-		'Win Odds'  =>  '4, 11, 7, 5, 3, 12, 2, 10, 6, 8, 1, 9',
-		'Pla Odds'  =>  '11, 4, 7, 5, 3, 2, 10, 12, 8, 6, 1, 9',
+		'Win Odds'  =>  '4, 11, 7, 5, 3, 2, 12, 10, 8, 6, 1, 9',
+		'Pla Odds'  =>  '11, 4, 7, 5, 3, 2, 12, 10, 8, 6, 9, 1',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		/** Black selection */
@@ -72,7 +72,7 @@ return [
 		Race 6
 		*/
 		'Win Odds'  =>  '4, 9, 5, 8, 3, 12, 7, 11, 6, 1, 2, 10',
-		'Pla Odds'  =>  '4, 5, 3, 9, 7, 12, 11, 8, 6, 1, 2, 10',
+		'Pla Odds'  =>  '4, 5, 3, 9, 7, 12, 11, 8, 6, 2, 1, 10',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		'win odds candidate(k = 7)' => '12',
@@ -87,16 +87,17 @@ return [
 		/**
 		Race 7
 		*/
-		'Win Odds'  =>  '4, 2, 3, 8, 10, 5, 11, 1, 6, 9, 7, 12',
-		'Pla Odds'  =>  '4, 10, 11, 3, 8, 2, 5, 1, 9, 7, 6, 12',
+		'Win Odds'  =>  '4, 2, 3, 8, 10, 5, 11, 1, 9, 6, 7, 12',
+		'Pla Odds'  =>  '10, 4, 11, 3, 8, 2, 5, 1, 9, 7, 6, 12',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		'win odds candidate(k = 12)' => '4',
+		'win odds candidate(k = 9)' => '8',
 		/** Black selection */
 		'history w' => '4, 8, 10',
 		'SURE PLACE' => '4',
 		/** Based on pla odds */
-		'pla odds candidate(k = 12)' => '4',
+		'pla odds candidate(k = 12)' => '10',
 		'pla odds candidate(k = 9)' => '3',
 		'history p' => '10, 4, 11, 8, 2',
 		'SURE PLACE' => '4, 2',
@@ -106,7 +107,7 @@ return [
 		Race 8
 		*/
 		'Win Odds'  =>  '9, 11, 10, 5, 3, 2, 1, 12, 8, 7, 6, 4',
-		'Pla Odds'  =>  '9, 11, 10, 3, 5, 12, 1, 2, 8, 7, 6, 4',
+		'Pla Odds'  =>  '9, 11, 10, 3, 5, 12, 2, 1, 8, 7, 6, 4',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		/** Black selection */
