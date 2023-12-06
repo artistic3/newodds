5e9ec3a81d0de08ae4a80226ffb68ab00efdbde5
artistic3 <87168661+artistic3@users.noreply.github.com>
2023-12-06 20:43:25 +0800

    Update trace.php

--- trace.php
+++ trace.php
@@ -32,8 +32,8 @@ foreach($output as $line){
         $history[]   = substr($line, strlen('commit'));
     }
 }
-//limit search to last 100 commits
-$history = array_slice($history, 0, 100);
+//limit search to last 40 commits
+$history = array_slice($history, 0, 40);
 exec("git config --global advice.detachedHead false");
 for($count = count($history); $count > 1; $count --){
     $oldVersion = $history[$count - 1];
